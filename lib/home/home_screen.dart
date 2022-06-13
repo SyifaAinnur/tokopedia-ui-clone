7e6551ebb3d6713b03979224bfe4885ff922be54
Syifa Ainnur <arwahitem01@gmail.com>
2022-06-13 11:20:15 +0700

    add animation appbar

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -11,49 +11,189 @@ import 'package:tokopedia_ui/home/widget/slideritem.dart';
 import 'package:tokopedia_ui/home/widget/servicesIcon.dart';
 import 'package:tokopedia_ui/models/location_services.dart';
 import 'package:tokopedia_ui/theme.dart';
+import 'package:curved_animation_controller/curved_animation_controller.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  void initState() {
-    super.initState();
-    LocationServices().getCoordinate();
-  }
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  ScrollController _scrollController = ScrollController();
+
+  late CurvedAnimationController<Color> _animationBackground;
+  late CurvedAnimationController<Color> _animationInput;
+  late CurvedAnimationController<Color> _animationIcon;
+
+  double get _systemBarHeight => MediaQuery.of(context).padding.top;
+  double get _appBarHeight => kToolbarHeight + _systemBarHeight;
+  double get _appBarPaddingVertical => 10;
+  double get _appBarPaddingTop => _systemBarHeight + _appBarPaddingVertical;
+  double get _appBarPaddingBottom => _appBarPaddingVertical;
+
+  Color _appbarBackgroundColorBegin = primaryColor.withOpacity(1);
+  Color _appbarBackgroundColorEnd = Colors.white;
+
+  Color _inputBackgroundColorBegin = Colors.white.withOpacity(0.92);
+  Color _inputBackgroundColorEnd = Color(0xFFEFEFEF);
+
+  Color _iconColorBegin = Colors.white.withOpacity(0.92);
+  Color _iconColorEnd = Colors.grey;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                Color(0xffE3E4E8),
-                Color(0xffADCE0),
-              ])),
-          child: Stack(children: [
-            ClipPath(
-              clipper: ClipPathClass(),
+  void initState() {
+    _initAnimation();
+    LocationServices().getCoordinate();
+    super.initState();
+    _initScroll();
+  }
+
+  _initAnimation() {
+    _animationBackground = CurvedAnimationController<Color>.tween(
+      ColorTween(
+          begin: _appbarBackgroundColorBegin, end: _appbarBackgroundColorEnd),
+      Duration(milliseconds: 200),
+      curve: Curves.ease,
+      vsync: this,
+    );
+
+    _animationInput = CurvedAnimationController<Color>.tween(
+      ColorTween(
+          begin: _inputBackgroundColorBegin, end: _inputBackgroundColorEnd),
+      Duration(milliseconds: 300),
+      curve: Curves.ease,
+      vsync: this,
+    );
+
+    _animationIcon = CurvedAnimationController<Color>.tween(
+      ColorTween(begin: _iconColorBegin, end: _iconColorEnd),
+      Duration(milliseconds: 300),
+      curve: Curves.ease,
+      vsync: this,
+    );
+
+    _animationBackground.addListener(() => setState(() {}));
+    _animationInput.addListener(() => setState(() {}));
+    _animationIcon.addListener(() => setState(() {}));
+  }
+
+  _initScroll() {
+    _scrollController.addListener(() {
+      double startAnimationAfterOffset = 5;
+      double scrollOffsetBackground = 150;
+      double scrollOffsetInput = 150;
+      double scrollOffsetIcon = 120;
+
+      // delay animation to start animate only after scrolling
+      // as far as startAnimationAfterOffset value
+      // this is for a smoother effect
+      double offset = _scrollController.offset - startAnimationAfterOffset;
+      double progressBackground = offset / scrollOffsetBackground;
+      double progressInput = offset / scrollOffsetInput;
+      double progressIcon = offset / scrollOffsetIcon;
+
+      // make sure progress animation always between 0.0 and 1.0
+      progressBackground = progressBackground <= 0.0 ? 0.0 : progressBackground;
+      progressBackground = progressBackground >= 1.0 ? 1.0 : progressBackground;
+
+      // make sure progress animation always between 0.0 and 1.0
+      progressInput = progressInput <= 0.0 ? 0.0 : progressInput;
+      progressInput = progressInput >= 1.0 ? 1.0 : progressInput;
+
+      // make sure progress animation always between 0.0 and 1.0
+      progressIcon = progressIcon <= 0.0 ? 0.0 : progressIcon;
+      progressIcon = progressIcon >= 1.0 ? 1.0 : progressIcon;
+
+      _animationBackground.progress = progressBackground;
+      _animationInput.progress = progressInput;
+      _animationIcon.progress = progressIcon;
+    });
+  }
+  // void initState() {
+  //   super.initState();
+  //   LocationServices().getCoordinate();
+  // }
+
+  Widget get _appbar => Container(
+        height: _appBarHeight,
+        padding: EdgeInsets.only(
+          top: _appBarPaddingTop,
+          bottom: _appBarPaddingBottom,
+        ),
+        color: _animationBackground.value,
+        child: Row(
+          children: [
+            SizedBox(width: 15),
+            Expanded(
               child: Container(
-                height: 200,
-                color: primaryColor,
+                padding: EdgeInsets.symmetric(horizontal: 0),
+                decoration: BoxDecoration(
+                  color: _animationInput.value,
+                  borderRadius: BorderRadius.circular(4.0),
+                ),
+                child: TextField(
+                  textInputAction: TextInputAction.search,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    border: InputBorder.none,
+                    hintText: 'Search...',
+                    hintStyle: TextStyle(
+                      color: Colors.black38,
+                      fontSize: 14,
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.black54, fontSize: 14),
+                ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(top: 40),
-              child: CustomAppBar(),
+            SizedBox(width: 13),
+            IconButton(
+              icon: Icon(Icons.favorite, color: _animationIcon.value),
+              onPressed: () {},
             ),
+            IconButton(
+              icon: Icon(Icons.mail, color: _animationIcon.value),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon:
+                  Icon(Icons.notifications_active, color: _animationIcon.value),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      );
+
+  Widget get _content => SingleChildScrollView(
+        controller: _scrollController,
+        child: Column(
+          children: [
             Container(
-              child: ListView(children: [
-                Container(
-                  margin: EdgeInsets.only(top: 40),
-                  child: Column(
-                    children: [
+              height: 3000,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                    Color(0xffE3E4E8),
+                    Color(0xffADCE0),
+                  ])),
+              child: Stack(
+                children: [
+                  ClipPath(
+                    clipper: ClipPathClass(),
+                    child: Container(
+                      height: 200,
+                      color: primaryColor,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 90),
+                    child: Column(children: [
                       SizedBox(
                         height: 20,
                       ),
@@ -111,34 +251,35 @@ class _HomeScreenState extends State<HomeScreen> {
                           ],
                         ),
                       ),
-                      Container(
-                        child: ServiceIcon(),
-                      ),
-                      SizedBox(height: 5),
-                      Positioned(
-                        child: Menutop(),
-                      ),
-                      Container(
-                        child: SliderItem(),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Container(
-                        child: Menubottom(),
-                      ),
-                      Container(
-                        child: FlashSale(),
-                      ),
-                    ],
-                  ),
-                ),
-              ]),
+                      
+                      ServiceIcon(),
+                      SizedBox(height: 10),
+                      Menutop(),
+                      SliderItem(),
+                      SizedBox(height: 10),
+                      Menubottom(),
+                      SizedBox(height: 10),
+                      FlashSale()
+                    ]),
+                  )
+                ],
+              ),
             ),
-          ]),
+            // Container(height: 1, color: Color(0xFF000046)),
+          ],
         ),
-          bottomNavigationBar: MainBottomNavbar(),               
-        );
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          _content,
+          _appbar,
+        ],
+      ),
+    );
   }
 }
 
